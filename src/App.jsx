4220631e10072681e6e605642aad88acd4b5ad8a
Paulo Junior <paulojpjunior@gmail.com>
2024-07-09 21:12:17 -0300

    Parte Lógica
    Criação da parte lógica.

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,20 @@ import Title from "./components/Title";
 import "./App.css";
 import Fireworks from "./assets/fireworks-574739_1280.jpg";
 
+import useCountdown from "./hocks/useCountdown";
+
 function App() {
+const [day, hour, minute, second] = useCountdown("Jan 1, 2025 00:00:00");
+
   return (
     <div className="App" style={{ backgroundImage: `url(${Fireworks})` }}>
       <div className="container">
         <Title title="Contagem regressiva para 2025" />
         <div className="countdown-container">
-          <Counter title="Dias" number={2} />
-          <Counter title="Horas" number={2} />
-          <Counter title="Minutos" number={2} />
-          <Counter title="Segundos" number={2} />
+          <Counter title="Dias" number={day} />
+          <Counter title="Horas" number={hour} />
+          <Counter title="Minutos" number={minute} />
+          <Counter title="Segundos" number={second} />
         </div>
       </div>
     </div>
